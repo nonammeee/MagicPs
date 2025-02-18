2ce56e784150a8886f1348834ace9b5503821fe8
nonammeee <97672364+nonammeee@users.noreply.github.com>
2025-02-18 20:04:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("/api/stats")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("playerCount").textContent = data.players;
+        });
+});
